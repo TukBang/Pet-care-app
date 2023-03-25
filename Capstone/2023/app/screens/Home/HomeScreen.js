64902df216fb679be9908575ae06bf97b9878f76
GoRuSHiP <86669160+ShotRin@users.noreply.github.com>
2023-03-25 14:58:24 +0900

    login

--- Capstone/2023/app/screens/Home/HomeScreen.js
+++ Capstone/2023/app/screens/Home/HomeScreen.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
+import { useUserContext } from "../../contexts/UserContext";
+
+// const {user} = useUserContext();
 
 function HomeScreen() {
-    return <View style={styles.block} />;
+    return (
+    <View style={styles.block}>
+        {/* {user.photoURL && (
+        <Image
+          source={{uri: user.photoURL}}
+          style={{width: 128, height: 128, marginBottom: 16}}
+          resizeMode="cover"
+        />
+      )} */}
+    </View>
+    )
 }
 
 const styles = StyleSheet.create({
