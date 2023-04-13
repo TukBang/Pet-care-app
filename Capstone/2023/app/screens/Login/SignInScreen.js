731def162a17e15109c61e3c33d45892383881f7
RohHanYoung <104245698+RohHanYoung@users.noreply.github.com>
2023-04-13 16:21:42 +0900

    pet_info and uid save information

--- Capstone/2023/app/screens/Login/SignInScreen.js
+++ Capstone/2023/app/screens/Login/SignInScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Keyboard,
   KeyboardAvoidingView,
@@ -6,109 +6,109 @@ import {
   StyleSheet,
   Text,
   View,
-  Alert
+  Alert,
 } from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import SignButtons from '../../components/Login/SignButtons';
 import SignInForm from '../../components/Login/SignForm';
 import { useUserContext } from '../../contexts/UserContext';
 import { signIn, signUp } from '../../lib/auth';
 import { getUser } from '../../lib/users';
 
+function SignInScreen({ navigation, route }) {
+  const { isSignUp } = route.params || {};
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const [loading, setLoading] = useState(false);
+  const { setUser } = useUserContext();
 
-function SignInScreen({navigation, route, confirmPassword}) {
-    const {isSignUp} = route.params || {};
-    const [form, setForm] = useState({
-      email: '',
-      password: '',
-      confirmPassword: '',
-    });
-
-    const [loading, setLoading] = useState();
-    const {setUser} = useUserContext();
-
-  
-    const createChangeTextHandler = (name) => (value) => {
-      setForm({...form, [name]: value});
+  useEffect(() => {
+    return () => {
+      setLoading(false);
     };
+  }, []);
 
+  const createChangeTextHandler = (name) => (value) => {
+    setForm({ ...form, [name]: value });
+  };
 
+  const onSubmit = async () => {
+    Keyboard.dismiss();
+    const { email, password, confirmPassword } = form;
 
-    const onSubmit = async () => {
-        Keyboard.dismiss();
-        const {email, password, confirmPassword} = form;
+    if (isSignUp && password !== confirmPassword) {
+      Alert.alert('실패', '비밀번호가 일치하지 않습니다.');
+      console.log({ password, confirmPassword });
+      return;
+    }
 
-        if (isSignUp && password !== confirmPassword) {
-            Alert.alert('실패', '비밀번호가 일치하지 않습니다.');
-            console.log({password, confirmPassword});
-            return;
-          }
+    setLoading(true);
+    const info = { email, password };
 
-        setLoading(true);
-        const info = {email, password};
-        
-        try {
-          const {user} = isSignUp ? await signUp(info) : await signIn(info);
-            const profile = await getUser(user.uid);
-            if (!profile) {
-            navigation.navigate('Welcome', {uid: user.uid});
-            } else {
-              setUser(profile);
-            }
-        } catch (e) {
-            const messages = {
-                'auth/email-already-in-use': '이미 가입된 이메일입니다.',
-                'auth/wrong-password': '잘못된 비밀번호입니다.',
-                'auth/user-not-found': '존재하지 않는 계정입니다.',
-                'auth/invalid-email': '유효하지 않은 이메일 주소입니다.',
-              };
-              const msg = messages[e.code] || `${isSignUp ? '가입' : '로그인'} 실패`;
-              Alert.alert('실패', msg);
-        } finally {
-          setLoading(false);
-        }
+    try {
+      const { user } = isSignUp ? await signUp(info) : await signIn(info);
+      const profile = await getUser(user.uid);
+      if (!profile) {
+        navigation.navigate('Welcome', { uid: user.uid });
+      } else {
+        setUser(profile);
+      }
+    } catch (e) {
+      const messages = {
+        'auth/email-already-in-use': '이미 가입된 이메일입니다.',
+        'auth/wrong-password': '잘못된 비밀번호입니다.',
+        'auth/user-not-found': '존재하지 않는 계정입니다.',
+        'auth/invalid-email': '유효하지 않은 이메일 주소입니다.',
       };
-  
-    return (
-      <KeyboardAvoidingView
-        style={styles.keyboardAvoidingView}
-          behavior={Platform.select({ios: 'padding'})}>
-          <SafeAreaView style={styles.fullscreen}>
-            <Text style={styles.text}>Welcome to PCA</Text>
-            <View style={styles.form}>
-              <SignInForm
-                isSignUp={isSignUp}
-                onSubmit={onSubmit}
-                form={form}
-                createChangeTextHandler={createChangeTextHandler}
-              />
-              <SignButtons isSignUp={isSignUp} onSubmit={onSubmit} loading={loading} />
-            </View>
-         </SafeAreaView>
-       </KeyboardAvoidingView>
-    );
-  }
+      const msg = messages[e.code] || `${isSignUp ? '가입' : '로그인'} 실패`;
+      Alert.alert('실패', msg);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <KeyboardAvoidingView
+      style={styles.keyboardAvoidingView}
+      behavior={Platform.select({ ios: 'padding' })}>
+      <SafeAreaView style={styles.fullscreen}>
+        <Text style={styles.text}>Welcome to PCA</Text>
+        <View style={styles.form}>
+          <SignInForm
+            isSignUp={isSignUp}
+            onSubmit={onSubmit}
+            form={form}
+            createChangeTextHandler={createChangeTextHandler}
+          />
+          <SignButtons isSignUp={isSignUp} onSubmit={onSubmit} loading={loading} />
+        </View>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
+  );
+}
 
 const styles = StyleSheet.create({
-    keyboardAvoidingView: {
-        flex: 1,
-      },
-    fullscreen: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent:'center'
-    },
-    text: {
-        fontSize: 32,
-        fontWeight: 'bold',
-        color: 'black'
-    },
-    form: {
-        marginTop: 64,
-        width: '100%',
-        paddingHorizontal: 16,
-    },
+  keyboardAvoidingView: {
+    flex: 1,
+  },
+  fullscreen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  form: {
+    marginTop: 64,
+    width: '100%',
+    paddingHorizontal: 16,
+  },
+});
 
-})
-
-export default SignInScreen;
+export default SignInScreen
