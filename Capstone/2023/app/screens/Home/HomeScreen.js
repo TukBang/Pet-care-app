c5d052be37adfc7e98d49280cf7c72ef26f35673
RohHanYoung <104245698+RohHanYoung@users.noreply.github.com>
2023-04-15 12:35:57 +0900

    connect server uid
    connect server uid

--- Capstone/2023/app/screens/Home/HomeScreen.js
+++ Capstone/2023/app/screens/Home/HomeScreen.js
@@ -1,3 +1,122 @@
+// import React, { useState, useEffect } from "react";
+// import { StyleSheet, View, Text, TextInput, Button } from "react-native";
+// import { useUserContext } from "../../contexts/UserContext";
+
+// function HomeScreen() {
+//   const [petname, setPetname] = useState("");
+//   const [gender, setGender] = useState("");
+//   const [weight, setWeight] = useState("");
+//   const [birth, setBirth] = useState("");
+//   const [kind, setKind] = useState("");
+//   const [saving, setSaving] = useState(false);
+//   const {user} = useUserContext();
+//   const uid = user["id"];
+  
+
+//   useEffect(() => {
+//     return () => {
+//       // cleanup function
+//       setSaving(false); // cancel saving if component unmounts
+//     };
+//   }, []);
+
+//   const savePet = async () => {
+//     setSaving(true);
+//     try {
+//       const response = await fetch("http://127.0.0.1:4000/pet", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//           petname,
+//           uid : user.id,
+//           gender,
+//           weight,
+//           birth,
+//           kind,
+//         }),
+//       });
+//       const data = await response.json();
+//       console.log(data);
+//     } catch (error) {
+//       console.error(error);
+//     } finally {
+//       // check if component is still mounted before updating state
+//       if (!saving) return;
+//       setSaving(false);
+//     }
+//   };
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>Add new pet information.</Text>
+//       <TextInput
+//         style={styles.input}
+//         placeholder="Pet name"
+//         onChangeText={(text) => setPetname(text)}
+//         value={petname}
+//       />
+//       <TextInput
+//         style={styles.input}
+//         placeholder="Gender"
+//         onChangeText={(text) => setGender(text)}
+//         value={gender}
+//       />
+//       <TextInput
+//         style={styles.input}
+//         placeholder="Weight"
+//         onChangeText={(text) => setWeight(text)}
+//         value={weight}
+//       />
+//       <TextInput
+//         style={styles.input}
+//         placeholder="Birth date"
+//         onChangeText={(text) => setBirth(text)}
+//         value={birth}
+//       />
+//       <TextInput
+//         style={styles.input}
+//         placeholder="Kind"
+//         onChangeText={(text) => setKind(text)}
+//         value={kind}
+//       />
+//       <TextInput
+//         style={styles.input}
+//         placeholder="User ID"
+//         onChangeText={() => {}}
+//         value={user.id}
+//         editable={false}
+//       />
+//       <Button title="Save pet information" onPress={savePet} />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   title: {
+//     fontSize: 24,
+//     fontWeight: "bold",
+//     marginBottom: 16,
+//   },
+//   input: {
+//     borderWidth: 1,
+//     borderColor: "#ccc",
+//     borderRadius: 4,
+//     padding: 8,
+//     margin: 8,
+//     width: "80%",
+//     maxWidth: 400,
+//   },
+// });
+
+// export default HomeScreen;
+
 import React, { useState, useEffect } from "react";
 import { StyleSheet, View, Text, TextInput, Button } from "react-native";
 import { useUserContext } from "../../contexts/UserContext";
@@ -10,7 +129,8 @@ function HomeScreen() {
   const [kind, setKind] = useState("");
   const [saving, setSaving] = useState(false);
   const {user} = useUserContext();
-  const uid = user.uid;
+
+  const uid = user["id"];
 
   useEffect(() => {
     return () => {
@@ -29,7 +149,7 @@ function HomeScreen() {
         },
         body: JSON.stringify({
           petname,
-          uid,
+          id: uid,
           gender,
           weight,
           birth,
@@ -84,7 +204,7 @@ function HomeScreen() {
         style={styles.input}
         placeholder="User ID"
         onChangeText={() => {}}
-        value={user.id}
+        value={uid}
         editable={false}
       />
       <Button title="Save pet information" onPress={savePet} />
