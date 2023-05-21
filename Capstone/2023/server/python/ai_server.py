a71dda8ffe52c04964f0714160b2232ed2be4a5d
TukBang <bang0224@tukorea.ac.kr>
2023-05-22 01:18:07 +0900

    server code

--- Capstone/2023/server/python/ai_server.py
+++ Capstone/2023/server/python/ai_server.py
@@ -3,11 +3,18 @@ import pickle as pkl
 import sys, os
 from pprint import *
 import numpy as np
+
+# AI
 import cv2
 import torch
 from torchvision import transforms
 from torchvision.models import efficientnet_v2_s
 from torchinfo import summary
+
+# ChatBot
+import openai
+import threading
+import time, datetime
 
 # Web server based on Flask ()
 from flask import Flask, jsonify, request, send_from_directory
@@ -24,14 +31,19 @@ import skin_disease.module.skin_disease_model as sdm
 # AI 불러오기
 # server AI model 가중치 저장 경로
 # Image 저장 경로
-model_path = "E:/Tukorea/Capstone/model/server/"
-image_path = "E:/Tukorea/Capstone/images/"
+model_path = "D:/Capstone/Capstone/model/server/"
+image_path = "D:/Capstone/Capstone/images/"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = sdm.Skin_Distinction_Model(model=efficientnet_v2_s(weights="DEFAULT"),
                                        out_features=6,
                                        device=device,
                                        save_path=model_path).to(device)
 
+# initial
+last_use_user = list()
+chatbot = dict()
+
+# history_fig 저장
 def save_history_fig(history):
     import math
     import numpy as np
@@ -87,6 +99,85 @@ def softmax(pred):
 
     return ret_pred
 
+# chatbot class
+class PetCareChatBot:
+    def __init__(self, model="gpt-3.5-turbo"):
+        self.model = model
+        self.last_use_time = datetime.datetime.now()
+        self.chatlog = dict()
+        self.chatlog["system"] = "You are a helpful assistant about pet care."
+        self.chatlog["user"] = list()
+        self.chatlog["question_token"] = [0]
+        self.chatlog["assistant"] = list()
+        self.chatlog["answer_token"] = [0]
+        self.chatlog["total_token"] = 0
+        self.SYSTEM_TOKEN_LEN = 22
+        return
+    
+    def return_create_time(self):
+        return (self.create_time.year, 
+                self.create_time.month, 
+                self.create_time.day, 
+                self.create_time.hour, 
+                self.create_time.minute, 
+                self.create_time.second)
+
+    def request_chat(self, sentence):
+        # initialize meesages
+        ## system message part
+        chat_messages = [{"role": "system", "content": self.chatlog["system"]}]
+        
+        ## user, assistant message part
+        for q, a in zip(self.chatlog["user"], self.chatlog["assistant"]):
+            chat_messages.append({"role": "user", "content": q})
+            chat_messages.append({"role": "assistant", "content": a})
+
+        # append question message
+        chat_messages.append({"role": "user", "content": sentence})
+        self.chatlog["user"].append(sentence)
+
+        # request message
+        openai.organization = os.getenv("OPENAI_ORGANIZATION")
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+        completion = openai.ChatCompletion.create(
+            model=self.model,
+            messages=chat_messages
+        )
+
+        # update token
+        self.chatlog["answer_token"].append(completion["usage"]["completion_tokens"])
+        if (len(self.chatlog["question_token"]) == 0):
+            self.chatlog["question_token"].append(
+                completion["usage"]["prompt_tokens"] - self.SYSTEM_TOKEN_LEN
+            )
+        else:
+            self.chatlog["question_token"].append(
+                completion["usage"]["prompt_tokens"] 
+                - sum(self.chatlog["question_token"][:-1])
+                - sum(self.chatlog["question_token"][:-1])
+                - self.SYSTEM_TOKEN_LEN
+            )
+        self.chatlog["total_token"] = completion["usage"]["total_tokens"]
+
+        # append answer message
+        answer_message = completion["choices"][0]["message"]["content"]
+        self.chatlog["assistant"].append(answer_message)
+
+        # delete exceed log messages
+        while self.chatlog["total_token"] > 1000:
+            # delete exceed token
+            question_token = self.chatlog["question_token"].pop(0)
+            answer_token = self.chatlog["answer_token"].pop(0)
+
+            ### part that can improve memory ability efficiency ###
+            self.chatlog["user"].pop(0)
+            self.chatlog["assistant"].pop(0)
+            self.chatlog["total_token"] -= question_token - answer_token
+            #######################################################
+        
+        self.last_use_time = datetime.datetime.now()
+        return answer_message
+
 class ImageResource(Resource):
     # 이 부분은 인공지능 평가에 대한 데이터가 보내져야 함
     def get(self):
@@ -138,9 +229,53 @@ class ImageResource(Resource):
         else:
             return {'error': 'No image data found'}, 400
 
+class ChatResource(Resource):
+    def get(self):
+        # ChatGPT Model 정보 전송
+        return
+    
+    def post(self):
+        global last_use_user, chatbot
+        data = request.get_json()
+        uid = data.get('uid', None)
+        message = data.get('message', None)
+        if not isinstance(message, type(None)) and not isinstance(uid, type(None)):
+            try:
+                # 챗봇 생성
+                if uid not in chatbot:
+                    chatbot[uid] = PetCareChatBot()
+                    last_use_user.append(uid)
+                
+                ret_message = chatbot[uid].request_chat(message)
+                last_use_user.remove(uid)
+                last_use_user.append(uid)
+
+                ret_data = jsonify({'message': ret_message})
+                return ret_data
+            except Exception as e:
+                print(e)
+                return {'error': str(e)}, 400
+        else:
+            return {"error": "Please check your sent message"}, 400
+
+# last_use_user must be sorted by usage time
+def free_chatbot(chatbot, last_use_user):
+    while True:
+        time.sleep(60)
+        now = datetime.datetime.now()
+        for uid in last_use_user:
+            if (now - chatbot[uid].last_use_time).seconds > 3600:
+                chatbot.pop(uid)
+                last_use_user.remove(uid)
+
 if __name__ == "__main__":
     if not os.path.exists(image_path):
         os.makedirs(image_path)
+
+    # chatbot 대화 내용 제거용 thread 생성
+    chatbot_thread = threading.Thread(target=free_chatbot, args=(chatbot, last_use_user))
+    chatbot_thread.daemon = True
+    chatbot_thread.start()
 
     pprint(summary(model, input_size=(1, 3, 224, 224), verbose=0))
     
@@ -160,6 +295,7 @@ if __name__ == "__main__":
     
     api = Api(app)
     api.add_resource(ImageResource, '/images')
+    api.add_resource(ChatResource, '/chatbot')
 
     # 웹 서버 열기
     app.run(host='0.0.0.0', debug=True)
